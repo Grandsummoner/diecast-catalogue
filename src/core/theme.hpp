4541c64b173aabe2f294eb353a2f3f5b90826285
Grandsummoner <gsnextninja@gmail.com>
2026-07-18 21:26:49 +0800

    Update theme.hpp

--- src/core/theme.hpp
+++ src/core/theme.hpp
@@ -1,1 +1,9 @@
-d
+#pragma once
+
+#include "types.hpp"
+
+// Themes Apply Styles
+void ApplyTheme(Theme theme);
+
+// Neumorphic progress bar
+void DrawNeumorphicProgressBar(float fraction, const ImVec2& size, ImU32 barBgColor, ImU32 fillColor);
